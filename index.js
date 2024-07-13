fe24c3b96df03ee1612672307eee3562b19af85b
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-07-12 19:16:37 -0500

    Funcion para consultar por id

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Configurar con express el servidor de la app
-const express  = require('express');
+const express = require('express');
 const app = express();
 
-app.set('port',3000);
+app.set('port', 3000);
 app.listen(3000);
 
 //Llamar al objeto mongoClient del componente modngodb
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 //Funcion asincrona para hacer las peticiones
-async function usar(){
+async function consultarTodo() {
     //Crear una instacion de de mongo client
     const client = new MongoClient("Aqui va mi cadena de conexion");
 
@@ -18,18 +18,43 @@ async function usar(){
         const conexion = await client.connect();
         const controlador = conexion.db().collection('users');
 
-        let  consulta;
+        let consulta;
 
         //Realizando la consulta
         consulta = await controlador.find().toArray();
-        console.log('Seleccion: ',consulta);
+        console.log('Seleccion: ', consulta);
 
 
-        
+
     } catch (error) {
-        console.error('Ocurrio el siguiente error: ',error);
+        console.error('Ocurrio el siguiente error: ', error);
     }
 
 }
 
-usar();
+async function consultarFiltro() {
+
+    //Crear una instacion de de mongo client
+    const client = new MongoClient("");
+
+    //Conexion para hacer peticiones
+    try {
+        const conexion = await client.connect();
+        const controlador = conexion.db().collection('users');
+
+        let consulta,filtro;
+
+        //Realizando la consulta con un filtro
+        filtro = {id: 11};
+        consulta = await controlador.find(filtro).toArray();
+        console.log('Seleccion: ', consulta);
+
+
+
+    } catch (error) {
+        console.error('Ocurrio el siguiente error: ', error);
+    }
+
+}
+
+consultarFiltro();
